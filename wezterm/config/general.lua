1d2ba584b2d196530b84687feddedd15829fd1cb
stephen <stephenloggedon@gmail.com>
2024-01-02 15:25:37 -0800

    Add wezterm config

---
+++ wezterm/config/general.lua
@@ -0,0 +1,55 @@
+---@class WezTerm
+local wez = require "wezterm"
+
+local icons = require "utils.icons" ---@class Icons
+local fun = require "utils.fun" ---@class Fun
+
+---@class Config
+local Config = {}
+
+Config.default_prog = { "wsl", "-d", "fedora39" }
+
+Config.launch_menu = {
+  {
+    label = icons.Pwsh .. " PowerShell V7",
+    args = {
+      "pwsh",
+      "-NoLogo",
+      "-ExecutionPolicy",
+      "RemoteSigned",
+      "-NoProfileLoadTime",
+    },
+    cwd = "~",
+  },
+  { label = icons.Pwsh .. " PowerShell V5", args = { "powershell" }, cwd = "~" },
+  { label = "Command Prompt", args = { "cmd.exe" }, cwd = "~" },
+  { label = icons.Git .. " Git bash", args = { "sh", "-l" }, cwd = "~" },
+}
+
+-- ref: https://wezfurlong.org/wezterm/config/lua/WslDomain.html
+Config.wsl_domains = {
+  {
+    name = "WSL:Ubuntu",
+    distribution = "Ubuntu",
+    username = "sravioli",
+    default_cwd = "/home/sRavioli",
+    default_prog = { "bash" },
+  },
+  {
+    name = "WSL:Alpine",
+    distribution = "Alpine",
+    username = "sravioli",
+    default_cwd = "/home/sravioli",
+  },
+}
+
+Config.default_cwd = fun.home
+
+-- ref: https://wezfurlong.org/wezterm/config/lua/SshDomain.html
+Config.ssh_domains = {}
+
+-- ref: https://wezfurlong.org/wezterm/multiplexing.html#unix-domains
+Config.unix_domains = {}
+
+return Config
+
---
+++ wezterm/wezterm.lua
@@ -0,0 +1,6 @@
+require "events.update-status"
+require "events.format-tab-title"
+require "events.new-tab-button-click"
+
+return require("utils.config"):new():add("config"):add "mappings"
+
